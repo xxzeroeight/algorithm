3ca5291fec6dea0206ab76b141278947d8d257f2
xxzeroeight <xxzeroeight@naver.com>
2025-08-03 14:37:08 +0900

    문제 수정(25-08-03)
    - 1373, 2153

--- easy/string/2153.py
+++ easy/string/2153.py
@@ -1,17 +1,10 @@
-# ===== 문제 정보 =====
-# 번호: 2153 | 문제: 소수 단어 | 난이도: Bronze 2
-# 카테고리: 문자열
-
 import math
 import sys
 
-# ===== 해결 전략 =====
-# 아이디어: 아스키 코드로 변환된 문자들의 합이 소수인지 판별
-
-# ===== 입력 처리 =====
+# [입력 처리]
 words = sys.stdin.readline().rstrip()
 
-# ===== 핵심 로직 =====
+# [핵심 로직]
 def is_prime(x):
     for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:
@@ -26,19 +19,25 @@ for word in words:
     else:
         res += ord(word) - 96
 
-# ===== 결과 출력 =====
+# [결과 출력]
 print("It is a prime word." if is_prime(res) else "It is not a prime word.")
 
-# ===== 테스트 케이스 =====
-# - 입력: UFRN
-# - 출력: It is a prime word.
+"""
+# 문제 정보
 
-# - 입력: contest
-# - 출력: It is not a prime word.
+문제: [소수 단어]
+출처: [백준 2153]
+난이도: [B2]
+분류: [문자열]
+"""
 
-# ===== 메모 =====
-# 주의사항
-# - "A"-"Z": 65~90
-# - "a"-"z": 97~122
+"""
+# 메모
 
-# 실수한 부분
+아이디어
+ - 아스키 코드로 변환된 문자들의 합이 소수인지 판별
+주의 사항
+ - "A"-"Z": 65~90
+ - "a"-"z": 97~122
+개선점
+"""
