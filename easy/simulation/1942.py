c427ea1e625c08a8fb196e8908cfdefe89662937
xxzeroeight <xxzeroeight@naver.com>
2025-08-23 23:41:52 +0900

    문제 추가(25-08-23)
    - 1942, 14920, 16917
    - README 파일에 문제 추가

---
+++ easy/simulation/1942.py
@@ -0,0 +1,24 @@
+"""
+
+
+예제 1:
+입력:
+출력:
+
+제약 조건:
+
+"""
+
+import sys
+
+# [입력 처리]
+for _ in range(3):
+    start, end = sys.stdin.readline().split()
+
+# [핵심 로직]
+    t1 = int("".join(start.split(":")))
+    t2 = int("".join(end.split(":")))
+
+# [결과 출력]
+
+# 2025-08-23, 시도: 2
